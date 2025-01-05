fcda0953bdbadd58beefd59ac3bee662625db50e
CabbitKheema <161156792+CabbitKheema@users.noreply.github.com>
2025-01-05 15:43:04 +0530

    Converting demo vite react counter app to work offline as a pwa
    - Created react swc project using vite
    - Creation of pwa using vite-plugin-pwa
    - PWA only works on https server. so i made use of https-localhost.
    - Implemented pwa for the existing demo react app (works offline now, loads everything as well)
    - Issue where i failed to load resources with vite-plugin-pwa in dev mode. Fixed by adding lines 11 - 14 in vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
