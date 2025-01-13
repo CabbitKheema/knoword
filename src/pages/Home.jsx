0aee18ce97ab0a455cdeb3f119f061d6f016dc5c
CabbitKheema <161156792+CabbitKheema@users.noreply.github.com>
2025-01-13 15:36:07 +0530

    Transition label for speech and camera, fixed icons, managed searching state
    - Added transitioning labels to new audio word search option
    - Fixed icons to ensure that all icons are of the same type (mostly "react-icons/fi” type)
    - Displayed ‘Coming soon…’ text upon clicking camera word search option
    - Added "searching state management" to disable all buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import { useEffect, useRef } from "react";
-import { FaMicrophone } from "react-icons/fa";
-// import { RiSpeakFill } from "react-icons/ri";
-import { FaArrowRight } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 import { useState } from "react";
 import { FindWordMeaning } from "../apis/FindWordMeaning";
-import { FaKeyboard } from "react-icons/fa";
-import { FaCamera } from "react-icons/fa";
+import { FaRegKeyboard } from "react-icons/fa";
+import { FiCamera } from "react-icons/fi";
 import { SlOptions } from "react-icons/sl";
 import { BiX } from "react-icons/bi";
-import { FiMic, FiUpload } from "react-icons/fi"; // Icons for buttons
+import { FiMic } from "react-icons/fi"; // Icons for buttons
 import { FiPlay, FiPause } from "react-icons/fi";
-import { FaStop } from "react-icons/fa";
-import { FaTrash } from "react-icons/fa";
+import { IoStopOutline } from "react-icons/io5";
+import { FiTrash } from "react-icons/fi";
 import { VoiceNoteToText } from "../apis/VoiceNoteToText";
 
 export default function Home() {
@@ -19,6 +17,7 @@ export default function Home() {
   const [activelyTyping, setActivelyTyping] = useState(false);
   const [resultText, setResultText] = useState("");
   const [inputText, setInputText] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
 
   // Active states of type of inputs
   const [inputOptionActiveState, setInputOptionActiveState] = useState(true);
@@ -60,6 +59,7 @@ export default function Home() {
       setIsRecording(true);
     } catch (error) {
       console.error("Error accessing microphone:", error);
+      setResultText("Error accessing the microphone. Please try again.");
     }
   };
 
@@ -114,28 +114,32 @@ export default function Home() {
   async function textSubmitted() {
     console.log("Text submitted.");
     try {
+      setIsSearching(true);
       await setResult(inputText);
+      setIsSearching(false);
     } catch (error) {
       console.error("Error fetching word meaning:", error);
       setResultText(
         "Error fetching the meaning of the word. Please try again."
       );
+      setIsSearching(false);
     }
   }
 
   async function audioSubmitted() {
     console.log("audio submitted.");
     try {
+      setIsSearching(true);
       const word = await VoiceNoteToText(recordedAudio.blob); // Await the asynchronous function
-      // setInputText("");
-      // setActivelyTyping(false);
       await setResult(word);
       cancelRecordedAudio();
+      setIsSearching(false);
     } catch (error) {
       console.error("Error fetching word meaning:", error);
       setResultText(
         "Error fetching the meaning of the word. Please try again."
       );
+      setIsSearching(false);
     }
   }
 
@@ -207,7 +211,7 @@ export default function Home() {
                 onClick={onKeyboardActive}
                 className="px-4  py-2.5 rounded-l-lg bg-neutral-800 hover:bg-neutral-900 border border-neutral-700/80 outline-neutral-700"
               >
-                <FaKeyboard />
+                <FaRegKeyboard />
               </button>
 
               <button
@@ -215,7 +219,7 @@ export default function Home() {
                 onClick={onMicActive}
                 className="px-4  py-2.5  bg-neutral-800 hover:bg-neutral-900 border-y border-neutral-700/80 outline-neutral-700 "
               >
-                <FaMicrophone />
+                <FiMic />
               </button>
 
               <button
@@ -223,7 +227,7 @@ export default function Home() {
                 onClick={onCameraActive}
                 className="px-4  py-2.5 rounded-r-lg bg-neutral-800 hover:bg-neutral-900 border border-neutral-700/80 outline-neutral-700 "
               >
-                <FaCamera />
+                <FiCamera />
               </button>
             </div>
           )}
@@ -233,7 +237,9 @@ export default function Home() {
               <label className="absolute top-2 left-8 text-neutral-400 w-full">
                 <span
                   className={`absolute transition-opacity duration-500 ease-in-out ${
-                    activelyTyping ? "opacity-0" : "opacity-100"
+                    activelyTyping || (activelyTyping && !isSearching)
+                      ? "opacity-0"
+                      : "opacity-100"
                   } truncate`}
                 >
                   Type the word
@@ -241,17 +247,28 @@ export default function Home() {
 
                 <span
                   className={`absolute transition-opacity duration-500 ease-in-out  truncate ${
-                    activelyTyping ? "opacity-100" : "opacity-0"
+                    activelyTyping && !isSearching ? "opacity-100" : "opacity-0"
                   } `}
                 >
-                  Enter/Click arrow to find meaning.
+                  Enter/Click arrow to find meaning
+                </span>
+                <span
+                  className={`absolute transition-opacity duration-500 ease-in-out ${
+                    isSearching ? "opacity-100" : "opacity-0"
+                  } truncate`}
+                >
+                  Searching...
                 </span>
               </label>
               <input
                 type="text"
                 id="word"
                 value={inputText}
-                className="w-full p-2 text-sm rounded-l-lg text-neutral-300 border border-neutral-700/80 bg-neutral-800 focus:outline-none"
+                className={`w-full p-2 text-sm rounded-l-lg ${
+                  isSearching
+                    ? "text-neutral-400 bg-neutral-900"
+                    : "text-neutral-300 bg-neutral-800"
+                } border border-neutral-700/80 focus:outline-none`}
                 placeholder="e.g. providence"
                 onChange={handleInputChange} // Trigger state change while typing
                 onKeyDown={(event) => {
@@ -259,14 +276,20 @@ export default function Home() {
                     textSubmitted();
                   }
                 }}
+                disabled={isSearching}
               />
               {activelyTyping && (
                 <button
                   type="button"
-                  className="px-4  py-2.5 rounded-r-lg bg-neutral-800 hover:bg-neutral-900 border border-neutral-700/80 outline-neutral-700"
+                  className={`px-4  py-2.5 rounded-r-lg ${
+                    isSearching
+                      ? "bg-neutral-900"
+                      : "bg-neutral-800 hover:bg-neutral-900"
+                  }  border border-neutral-700/80 outline-neutral-700`}
                   onClick={textSubmitted}
+                  disabled={isSearching}
                 >
-                  <FaArrowRight />
+                  <FiSearch />
                 </button>
               )}
               {!activelyTyping && (
@@ -283,6 +306,53 @@ export default function Home() {
 
           {micActiveState && (
             <div className="relative group flex justify-center px-8 pb-8 pt-10">
+              <label className="absolute top-2 w-full text-neutral-400">
+                <span
+                  className={`absolute flex w-full justify-center items-center transition-opacity duration-500 ease-in-out ${
+                    isRecording || recordedAudio !== null
+                      ? "opacity-0"
+                      : "opacity-100"
+                  } truncate`}
+                >
+                  <FiMic className="flex-shrink-0 mr-1" /> to record voice
+                </span>
+
+                <span
+                  className={`absolute flex w-full justify-center items-center transition-opacity duration-500 ease-in-out  truncate ${
+                    isRecording ? "opacity-100" : "opacity-0"
+                  } `}
+                >
+                  <IoStopOutline className="flex-shrink-0 mr-1" /> to stop
+                  recording
+                </span>
+                <span
+                  className={`absolute flex w-full justify-center items-center transition-opacity duration-500 ease-in-out ${
+                    isPlaying ||
+                    recordedAudio === null ||
+                    isRecording ||
+                    isSearching
+                      ? "opacity-0"
+                      : "opacity-100"
+                  } truncate`}
+                >
+                  <FiPlay className="flex-shrink-0 mr-1" /> to replay recording
+                </span>
+
+                <span
+                  className={`absolute flex w-full justify-center items-center transition-opacity duration-500 ease-in-out  truncate ${
+                    isPlaying ? "opacity-100" : "opacity-0"
+                  } `}
+                >
+                  <FiPause className="flex-shrink-0 mr-1" /> to pause replay
+                </span>
+                <span
+                  className={`absolute flex w-full justify-center items-center transition-opacity duration-500 ease-in-out  truncate ${
+                    isSearching ? "opacity-100" : "opacity-0"
+                  } `}
+                >
+                  Searching...
+                </span>
+              </label>
               {/*Audio recording/stop button*/}
               <button
                 type="button"
@@ -290,13 +360,13 @@ export default function Home() {
                   isRecording ? stopRecording() : startRecording()
                 }
                 className={`px-4  py-2.5 rounded-l-lg border ${
-                  isPlaying
+                  isPlaying || isSearching
                     ? "bg-neutral-900"
                     : "bg-neutral-800 hover:bg-neutral-900"
                 } border-neutral-700/80 outline-neutral-700 `}
-                disabled={isPlaying}
+                disabled={isPlaying || isSearching}
               >
-                {isRecording ? <FaStop /> : <FiMic />}
+                {isRecording ? <IoStopOutline /> : <FiMic />}
               </button>
 
               {/*Recorded audio play/pause button*/}
@@ -305,11 +375,11 @@ export default function Home() {
                   type="button"
                   onClick={togglePlay}
                   className={`px-4  py-2.5  ${
-                    isRecording
+                    isRecording || isSearching
                       ? "bg-neutral-900"
                       : "bg-neutral-800 hover:bg-neutral-900"
                   }  border-y border-neutral-700/80 outline-neutral-700`}
-                  disabled={isRecording}
+                  disabled={isRecording || isSearching}
                 >
                   {isPlaying ? <FiPause /> : <FiPlay />}
                   {/* Hidden audio reference  */}
@@ -330,31 +400,33 @@ export default function Home() {
                   type="button"
                   onClick={cancelRecordedAudio}
                   className={`absolute bottom-0.5 p-1.5 rounded-full  ${
-                    isRecording || isPlaying
+                    isRecording || isPlaying || isSearching
                       ? "bg-neutral-900"
                       : "bg-neutral-800 hover:bg-neutral-900"
                   } border border-neutral-700/80 outline-neutral-700`}
-                  disabled={isPlaying || isRecording}
+                  disabled={isPlaying || isRecording || isSearching}
                 >
-                  <FaTrash className="size-3" />
+                  <FiTrash className="size-3" />
                   {/* This is the vertical line connecting delete and play button */}
-                  <div className="absolute left-2.5 bottom-4 -z-10">|</div>
+                  <div className="absolute left-2.5 bottom-4 -z-10 text-neutral-600">
+                    |
+                  </div>
                 </button>
               )}
 
-              {/*Recorded audio upload button*/}
+              {/*Recorded audio search button*/}
               {recordedAudio !== null && (
                 <button
                   type="button"
                   onClick={audioSubmitted}
                   className={`px-4  py-2.5 rounded-r-lg ${
-                    isRecording || isPlaying
+                    isRecording || isPlaying || isSearching
                       ? "bg-neutral-900"
                       : "bg-neutral-800 hover:bg-neutral-900"
                   } border  border-neutral-700/80 outline-neutral-700`}
-                  disabled={isPlaying || isRecording}
+                  disabled={isPlaying || isRecording || isSearching}
                 >
-                  <FiUpload />
+                  <FiSearch />
                 </button>
               )}
 
@@ -373,6 +445,29 @@ export default function Home() {
                   <SlOptions />
                 </button>
               )}
+            </div>
+          )}
+
+          {cameraActiveState && (
+            <div className="relative group flex justify-center px-8 pb-8 pt-10">
+              <label className="absolute top-2 text-neutral-400 w-full flex justify-center text-center">
+                <span className="absolute transition-opacity duration-500 ease-in-out opacity-100 group-hover:opacity-0 group-focus-within:opacity-0 truncate">
+                  Undergoing updates
+                </span>
+                <span className="absolute transition-opacity duration-500 ease-in-out opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 truncate">
+                  Delivering ASAP!
+                </span>
+              </label>
+              <p className="flex items-center text-sm text-neutral-300 border border-r-0 bg-neutral-800 rounded-l-lg border-neutral-700/80 py-2.5 px-4">
+                Coming soon...
+              </p>
+              <button
+                type="button"
+                onClick={onInputOptionActive}
+                className="px-4  py-2.5 rounded-r-lg bg-neutral-800 hover:bg-neutral-900 border  border-neutral-700/80 outline-neutral-700"
+              >
+                <SlOptions />
+              </button>
             </div>
           )}
 
