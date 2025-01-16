3065d5e030bb3f5be00f3ad1038bfe4fed09c308
CabbitKheema <161156792+CabbitKheema@users.noreply.github.com>
2025-01-17 02:21:11 +0530

    Code refactoring 3: Moving "text->meaning" service to backend project, added react-hook-form for handling form operations
    - Moved word meaning finder api to backend. Will be calling backend api for word text to meaning service from now on
    - changed dev hosting port to 5000
    - Used react-hook-form to create and handle form submissions along with form data validation and frontend error handling (used a custom error popup for validation errors)
    - completely changed SearchByText.jsx to call only backend api for the "find word meaning" service

--- src/components/SearchInput/SearchByText.jsx
+++ src/components/SearchInput/SearchByText.jsx
@@ -1,11 +1,7 @@
 import { FiSearch } from "react-icons/fi";
 import BackToOptions from "./Buttons/BackToOptions";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setTextResult } from "../../features/searchword/textResultSlice";
-import { FindWordMeaning } from "../../apis/FindWordMeaning";
-import { websiteAction } from "../../enums/enums";
-import { setWebsiteAction } from "../../features/websiteActions/websiteActionSlice";
 import {
   borderColor,
   leftFadingAbsoluteLabel,
@@ -21,42 +17,62 @@ import {
   idleTextSize,
 } from "../../constants";
 import { useToast } from "../Toast/ToastService";
+import { useForm } from "react-hook-form";
 
 export default function SearchByText() {
-  const currentWebsiteAction = useSelector(
-    (state) => state.websiteActionReducer.websiteAction
-  );
-  const isSearching = currentWebsiteAction == websiteAction.SEARCHING;
-  const [inputText, setInputText] = useState("");
-  const isTyping = inputText.trim().length;
-
   const dispatch = useDispatch();
   const toast = useToast();
 
-  const findInputTextMeaning = () => {
-    console.log("Searching word...");
-    dispatch(setWebsiteAction(websiteAction.SEARCHING));
-    FindWordMeaning(inputText)
-      .then((response) => {
-        toast.notificationToast(response);
-        dispatch(setTextResult(response.data)); // Update the state with the resolved result
-        setInputText("");
-      })
-      .catch((response) => {
-        console.error("Error fetching word meaning:", response.error);
-        toast.notificationToast(response);
-      })
-      .finally(() => {
-        dispatch(setWebsiteAction(websiteAction.IDLE));
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const inputText = watch("inputText", "");
+  const isTyping = inputText.trim().length > 0;
+
+  const onSubmit = async (formData) => {
+    try {
+      const res = await fetch(
+        import.meta.env.VITE_BACKEND_URL + "/find-word-definition",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.error("Error fetching word meaning:", data.error);
+        toast.notificationToast(data);
+      } else if (res.ok) {
+        dispatch(setTextResult(data.data));
+        toast.notificationToast(data);
+        reset();
+      } else if (res.status == 400) {
+        toast.notificationToast(data);
+      }
+    } catch (error) {
+      toast.notificationToast({
+        message: ["Error!", error.message || "Something went wrong."],
       });
+    }
   };
 
   return (
-    <div className={searchOptionStyle}>
+    <form className={searchOptionStyle} onSubmit={handleSubmit(onSubmit)}>
       <label className={leftFadingAbsoluteLabel}>
         <span
           className={`${fadingAbsoluteLabelSpan} ${
-            isTyping || (isTyping && !isSearching) ? "opacity-0" : "opacity-100"
+            isTyping ||
+            (isTyping && !isSubmitting) ||
+            (errors.inputText && !(errors.inputText.type === "required"))
+              ? "opacity-0"
+              : "opacity-100"
           }`}
         >
           Type the word
@@ -64,14 +80,16 @@ export default function SearchByText() {
 
         <span
           className={`${fadingAbsoluteLabelSpan} ${
-            isTyping && !isSearching ? "opacity-100" : "opacity-0"
+            isTyping && !isSubmitting && !errors.inputText
+              ? "opacity-100"
+              : "opacity-0"
           }`}
         >
           Enter/Click arrow to find meaning
         </span>
         <span
           className={`${fadingAbsoluteLabelSpan} ${
-            isSearching ? "opacity-100" : "opacity-0"
+            isSubmitting ? "opacity-100" : "opacity-0"
           }`}
         >
           Searching...
@@ -79,36 +97,48 @@ export default function SearchByText() {
       </label>
       <input
         type="text"
-        id="word"
-        value={inputText}
-        onChange={(e) => {
-          setInputText(e.target.value);
-        }}
-        className={`p-2 ${leftInteractableEdgeStyle} ${idleTextSize} ${
-          isSearching ? idleDisabledText : idleActiveText
-        }   ${borderColor} w-full focus:outline-none`}
+        {...register("inputText", {
+          maxLength: 100,
+          required: true,
+          validate: (value) =>
+            value.trim().length > 0 || "Empty spaces are not allowed",
+        })}
+        className={`p-2 ${leftInteractableEdgeStyle} ${idleTextSize} 
+        ${isSubmitting ? idleDisabledText : idleActiveText}   
+        ${borderColor} 
+        ${
+          errors.inputText && !(errors.inputText.type === "required")
+            ? "border-red-500"
+            : ""
+        } 
+        w-full focus:outline-none `}
         placeholder="e.g. providence"
-        onKeyDown={(event) => {
-          if (event.key === "Enter" && isTyping) {
-            findInputTextMeaning();
-          }
-        }}
-        disabled={isSearching}
+        disabled={isSubmitting}
       />
+      {errors.inputText && !(errors.inputText.type === "required") && (
+        <div className="absolute bottom-16 flex-1 justify-center items-center">
+          <p className="max-w-52 text-xs text-center bg-neutral-900 border border-red-500 rounded-lg p-2">
+            {errors.inputText.type === "maxLength" &&
+              "Maximum of 100 letters allowed"}
+            {errors.inputText.type === "required" && "This should not be empty"}
+            {errors.inputText.message}
+          </p>
+          <div className="-mt-1 text-red-600 flex justify-center">|</div>
+        </div>
+      )}
       {isTyping ? (
         <button
-          type="button"
+          type="submit"
           className={`${
-            isSearching ? hoverOrDisabledInteractableBG : idleInteractableBG
+            isSubmitting ? hoverOrDisabledInteractableBG : idleInteractableBG
           } ${interactablePadding} ${borderColor} ${rightInteractableEdgeStyle} border-l-0`}
-          onClick={findInputTextMeaning}
-          disabled={isSearching}
+          disabled={isSubmitting}
         >
           <FiSearch />
         </button>
       ) : (
         <BackToOptions />
       )}
-    </div>
+    </form>
   );
 }
