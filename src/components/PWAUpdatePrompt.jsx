a34224fd1f074992a438d4d8511b2d94e769cd85
CabbitKheema <mrprajwalkrishnamurthy@gmail.com>
2025-01-30 00:22:39 +0530

    Enhanced PWA update prompt and solved related bugs
    - Added a new single button prompt toast
    - Aligned toasts to display on top right of screen
    - The toast array is reversed such that recent toasts stay on the top
    - The cancel time for single button prompt toast is set to Infinity for now (This disables auto deletion of toast)
    - Prompt toast receives callbacks as argument when it is called (This is triggered on button onClick() event)
    - Removed triggering install update prompt via useState and React components, using toast instead.
    - Used useRef(bool) to ensure that update prompt was shown only once. This fixed the issue where update prompt was called multiple times on finding new PWA update

--- src/components/PWAUpdatePrompt.jsx
+++ src/components/PWAUpdatePrompt.jsx
@@ -1,73 +1,38 @@
-import { useState } from "react";
+import { useCallback, useRef } from "react";
 import { registerSW } from "virtual:pwa-register";
-import {
-  borderColor,
-  hoverOrDisabledInteractableBG,
-  idleActiveText,
-  idleInteractableBG,
-  idleTextSize,
-  roundedInteractableEdgeStyle,
-} from "../constants/index";
+import { useToast } from "./Toast/ToastService";
 
 export default function PWAUpdatePrompt() {
-  const [isUpdatePresent, setIsUpdatePresent] = useState(false);
+  const updatePromptRef = useRef(false);
+  const toast = useToast();
 
   // Use a flag to prevent multiple invocations
   const updateSW = registerSW({
     onNeedRefresh() {
-      // if (!isUpdatePresent) {
       // Ensure state updates only once
-      console.log("Update is available");
-      setIsUpdatePresent(true);
-      // }
+      if (!updatePromptRef.current) {
+        updatePromptRef.current = true;
+        console.log("Update is available");
+        toast.singlePromptToast(
+          {
+            message: ["Update available!", "Click 'Update' to install updates"],
+          },
+          handleRefresh
+        );
+      }
     },
     onOfflineReady() {},
   });
 
-  const handleRefresh = () => {
+  const handleRefresh = useCallback(() => {
     try {
       console.log("Build and SW updated");
       updateSW();
-      setIsUpdatePresent(false); // Hide prompt after update
     } catch (error) {
       console.error("Error updating service worker:", error);
+      toast.notificationToast({
+        message: ["Error!", "Error updating service worker"],
+      });
     }
-  };
-
-  // const handleCancel = () => {
-  //   console.log("Build and SW update canceled");
-  //   setIsUpdatePresent(false); // Properly reset the state
-  // };
-
-  return (
-    <section className="absolute top-20 right-8">
-      {isUpdatePresent && (
-        <div
-          className={`grid p-3 ${borderColor} ${hoverOrDisabledInteractableBG} ${roundedInteractableEdgeStyle}`}
-        >
-          <h2 className="text-md text-center font-semibold">
-            New update!
-            <br />
-            Refresh Service Worker
-          </h2>
-          <div className="pt-3 flex justify-center gap-2">
-            <button
-              className={`py-1 px-3 ${idleTextSize} ${idleActiveText}  ${borderColor} ${roundedInteractableEdgeStyle} ${idleInteractableBG}`}
-              onClick={handleRefresh}
-              aria-label="Refresh the service worker"
-            >
-              Refresh
-            </button>
-            {/* <button
-              className={buttonClass}
-              onClick={handleCancel}
-              aria-label="Cancel the refresh action"
-            >
-              Cancel
-            </button> */}
-          </div>
-        </div>
-      )}
-    </section>
-  );
+  }, [toast, updateSW]);
 }
--- src/components/Toast/ToastProvider.jsx
+++ src/components/Toast/ToastProvider.jsx
@@ -13,35 +13,64 @@ import {
 
 export default function ToastProvider({ children }) {
   const [toasts, setToasts] = useState([]);
-  const open = (component, timeout = 5000) => {
-    const id = Date.now();
-    setToasts((toasts) => [...toasts, { id, component }]);
-    setTimeout(() => close(id), timeout);
+  const open = (component, id, timeout = 5000) => {
+    setToasts((toasts) => [{ id, component }, ...toasts]);
+
+    if (timeout !== Infinity) setTimeout(() => close(id), timeout);
     return id;
   };
   const close = (id) =>
     setToasts((toasts) => toasts.filter((toast) => toast.id !== id));
 
   // Toasts
-  const notificationToast = (response) =>
+  const notificationToast = (response) => {
+    const id = Date.now();
     open(
       <div
-        className={`${borderColor} ${hoverOrDisabledInteractableBG} ${roundedInteractableEdgeStyle} flex fap-2 p-4 `}
+        className={`${borderColor} ${hoverOrDisabledInteractableBG} ${roundedInteractableEdgeStyle} flex gap-2 p-4 `}
       >
         <div>
           <h3 className="font-bold">{response.message[0]}</h3>
           <p className="text-sm">{response.message[1]}</p>
         </div>
-      </div>
+      </div>,
+      id
     );
+  };
+  const singlePromptToast = (response, installUpdatesCallback) => {
+    const id = Date.now();
+    open(
+      <div
+        className={`${borderColor} ${hoverOrDisabledInteractableBG} ${roundedInteractableEdgeStyle} flex gap-2 p-4 `}
+      >
+        <div>
+          <h3 className="font-bold">{response.message[0]}</h3>
+          <p className="text-sm">{response.message[1]}</p>
+          <button
+            onClick={() => {
+              close(id);
+              installUpdatesCallback();
+            }}
+            className={`py-1 px-3 mx-auto flex mt-2 ${idleTextSize} ${idleActiveText}  ${borderColor} ${roundedInteractableEdgeStyle} ${idleInteractableBG} `}
+          >
+            Update
+          </button>
+        </div>
+      </div>,
+      id,
+      Infinity
+    );
+  };
   //
 
   return (
-    <ToastContext.Provider value={{ open, close, notificationToast }}>
+    <ToastContext.Provider
+      value={{ open, close, notificationToast, singlePromptToast }}
+    >
       {children}
-      <div className="space-y-2 absolute bottom-20 right-4">
+      <div className="space-y-2 absolute top-20 right-4">
         {toasts.map(({ id, component }) => (
-          <div key={id} className="relative">
+          <div key={id} className="relative flex justify-end">
             <button
               onClick={() => close(id)}
               className={`${idleTextSize} ${idleActiveText}  ${borderColor} ${roundedInteractableEdgeStyle} ${idleInteractableBG} absolute top-2 right-2 p-1 `}
