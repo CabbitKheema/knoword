4fb0a01c1a666ca044d964225383236225d50f93
CabbitKheema <mrprajwalkrishnamurthy@gmail.com>
2025-01-30 19:44:26 +0530

    Manifest theme_color bug fix

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
         short_name: "knoWord",
         description:
           "This is a progressive web app to help book readers find meaning of unfamiliar words in the books they read quickly and effortlessly.",
+        theme_color: "#ffffff",
         icons: [
           {
             src: "/pwa-192x192.png",
